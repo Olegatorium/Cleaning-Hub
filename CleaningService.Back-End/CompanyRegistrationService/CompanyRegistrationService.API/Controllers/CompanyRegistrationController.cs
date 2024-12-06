9c4b19bb5b2982080d583975ce5219306a10165a
Oleh <olegatorium@gmail.com>
2024-12-07 00:25:31 +0100

    add MIT License

---
+++ CleaningService.Back-End/CompanyRegistrationService/CompanyRegistrationService.API/Controllers/CompanyRegistrationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyRegistrationService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyRegistrationController : ControllerBase
+    {
+        [HttpPost()]
+    }
+}
